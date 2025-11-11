cbd835fa228a690f50b9ca996c78a75c7b383156
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2025-11-11 21:53:38 +0200

    fix: import order

--- gripday-gateway-service/src/test/java/org/gripday/gatewayservice/service/ApiPrefixServiceTest.java
+++ gripday-gateway-service/src/test/java/org/gripday/gatewayservice/service/ApiPrefixServiceTest.java
@@ -1,5 +1,8 @@
 package org.gripday.gatewayservice.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 import org.gripday.gatewayservice.config.GatewayProperties;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,9 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("ApiPrefixService Tests")
