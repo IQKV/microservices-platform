3152f571be3e4ce95e13ec1ab6abc471d50f36aa
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-01-08 22:29:57 +0200

    style: remove unused import

--- iqscaffold-user-service/src/test/java/com/iqscaffold/userservice/infrastructure/messaging/UserEventPublisherTest.java
+++ iqscaffold-user-service/src/test/java/com/iqscaffold/userservice/infrastructure/messaging/UserEventPublisherTest.java
@@ -1,7 +1,6 @@
 package com.iqscaffold.userservice.infrastructure.messaging;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
